9ddde243ca65cf5e2830f48e08014766aebb5338
Ivan Aguas <aguas.xavier2013a@gmail.com>
2020-06-13 21:26:27 -0500

    Forward propagate input to a network output

--- main.py
+++ main.py
@@ -35,3 +35,18 @@ def transfer_tanh(x):
 
 def transfer_softmax(x):
     return np.exp(x)/np.sum(np.exp(x))
+
+
+# Forward propagate input to a network output
+def forward_propagate(network, row):
+	inputs = row
+	for layer in network:
+		new_inputs = []
+		for neuron in layer:
+			activation = activate(neuron['weights'], inputs)
+			neuron['output'] = transfer(activation)
+			new_inputs.append(neuron['output'])
+		inputs = new_inputs
+	return inputs
+
+
