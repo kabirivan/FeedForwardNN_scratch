e2458dd20fe0e0c22d44f7eb2045b7b2ab67a6f5
Ivan Aguas <aguas.xavier2013a@gmail.com>
2020-06-13 22:11:11 -0500

    Backpropagate error and store in neurons

--- main.py
+++ main.py
@@ -59,5 +59,22 @@ def forward_propagate(network, row):
     return inputs
 
 
-
+# Backpropagate error and store in neurons
+def backward_propagate_error(network, expected):
+	for i in reversed(range(len(network))):
+		layer = network[i]
+		errors = list()
+		if i != len(network)-1:
+			for j in range(len(layer)):
+				error = 0.0
+				for neuron in network[i + 1]:
+					error += (neuron['weights'][j] * neuron['delta'])
+				errors.append(error)
+		else:
+			for j in range(len(layer)):
+				neuron = layer[j]
+				errors.append(expected[j] - neuron['output'])
+		for j in range(len(layer)):
+			neuron = layer[j]
+			neuron['delta'] = errors[j] * transfer_derivative(neuron['output'])
 
